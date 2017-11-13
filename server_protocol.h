1094c85af3f581c167cdd9d434bc12a050ff35c8
erikavdiel <eri_pash@yahoo.com>
2017-11-13 18:39:20 +0200

    server_protocol.h - added macros that were approved in forum. might be better to define them in the bridge (network) part.
    server_protocol.c -
    implemented client serving which calls getNameAndFiles. left to
    implement a parser and execute to the command, let it return 0 if the
    command was quit, so the function will terminate.

--- server_protocol.h
+++ server_protocol.h
@@ -14,10 +14,24 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <dirent.h>
+
+#define MAX_USERNAME_SIZE 25
+#define MAX_PASSWORD_SIZE 25
+#define MAX_CLIENTS 15
+#define MAX_FILES_PER_CLIENT 15
+#define MAX_FILE_SIZE 512
+#define MAX_PATH_NAME 500
+#define MAX_FILE_NAME 50
+#define MAX_COMMAND_NAME 13
+
+
 typedef struct user_t {
 	const char* user_name;
 	const char* password;
 	const char* dir_path;
 } User;
+
+
 
 #endif /* SERVER_PROTOCOL_H_ */
