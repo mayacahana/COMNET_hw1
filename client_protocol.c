2916bfbc360a8a31778c22fe76d643cafda0c211
shawn <shawn@DESKTOP-CQT5K2M>
2017-11-16 15:43:36 +0200

    thursday, 4 pm

--- client_protocol.c
+++ client_protocol.c
@@ -18,12 +18,15 @@ char* getUserDetails() {
 	printf("No Line Read");
 	return NULL ;
 }
-
+/*
+ * @param pointer to string and size_t variable n
+ * removes last n bits from the string.
+ */
 void chopN(char *str, size_t n) {
 	assert(n != 0 && str != 0);
 	size_t len = strlen(str);
 	if (n > len)
-		return;  // Or: n = len;
+		return;
 	memmove(str, str + n, len - n + 1);
 }
 
@@ -44,8 +47,8 @@ int defineUser() {
 	char* passwordPrefix[11];
 	strncpy(userPrefix, fullUsername);
 	strncpy(passwordPrefix, fullPassword);
-	userPrefix[7] = "/0"; // "User: " if in correct format
-	passwordPrefix[11] = "/0"; // "Password: " if in correct format
+	userPrefix[7] = "\0"; // "User: " if in correct format
+	passwordPrefix[11] = "\0"; // "Password: " if in correct format
 	//check if input is in correct format and create message
 	if ((strcmp(userPrefix, "User: ") == 0)
 			&& (strcmp(passwordPrefix, "Password: ") == 0)) {
@@ -75,7 +78,8 @@ Message* createMessage(char* commandStr, MessageType type, char* prefix) {
 	m->type = type;
 	m->fromClient = 1;
 	m->arg1 = prefix;
-	m->arg2 = chopN(commandStr, strlen(prefix));
+	chopN(commandStr, strlen(prefix));
+	m->arg2 = commandStr;
 	return m;
 }
 
@@ -83,7 +87,7 @@ int sendCommand(char* commandStr) {
 	int status = 0;
 	char* inputPrefix[14];
 	strncpy(inputPrefix, commandStr);
-	inputPrefix[14] = "/0";
+	inputPrefix[14] = "\0";
 	if (strcmp(inputPrefix, "list_of_files") == 0) {
 		Message* m = createMessage(commandStr, LIST_OF_FILES, inputPrefix);
 		if (m != NULL ) {
@@ -93,7 +97,7 @@ int sendCommand(char* commandStr) {
 		printf("message not created");
 		return 1;
 	}
-	inputPrefix[11] = "/0";
+	inputPrefix[11] = "\0";
 	if (strcmp(inputPrefix, "delete_file") == 0) {
 		Message* m = createMessage(commandStr, DELETE_FILE, inputPrefix);
 		if (m != NULL ) {
@@ -103,7 +107,7 @@ int sendCommand(char* commandStr) {
 		printf("message not created");
 		return 1;
 	}
-	inputPrefix[8] = "/0";
+	inputPrefix[8] = "\0";
 	if (strcmp(inputPrefix, "add_file") == 0) {
 		Message* m = createMessage(commandStr, ADD_FILE, inputPrefix);
 		if (m != NULL ) {
@@ -122,7 +126,7 @@ int sendCommand(char* commandStr) {
 		printf("message not created");
 		return 1;
 	}
-	inputPrefix[4] = "/0";
+	inputPrefix[4] = "\0";
 	if (strcmp(inputPrefix, "quit") == 0) {
 		Message* m = createMessage(commandStr, QUIT, inputPrefix);
 		if (m != NULL ) {
@@ -138,8 +142,7 @@ int sendCommand(char* commandStr) {
 
 void startClient(char* hostname, int port) {
 	if (hostname == NULL ) {
-		hostname = malloc(sizeof(char) * 15);
-		hostname = "localhost";
+		hostname[10] = "localhost";
 		port = 2235;
 	} else if (port == 0)
 		port = 2235;
@@ -160,7 +163,7 @@ void startClient(char* hostname, int port) {
 
 	dest_addr.sin_family = AF_INET;
 	dest_addr.sin_port = htons(port);
-	dest_addr.sin_addr = htonl(0x8443FC64); // to do
+	//dest_addr.sin_addr = htonl(0x8443FC64); // to do
 
 	int connect;
 	connect = connect(socketfd, (struct sockaddr*)&server_addr, sizeof(struct server_addr));
@@ -169,7 +172,6 @@ void startClient(char* hostname, int port) {
 		printf("connection failed");
 		return;
 	}
-
 	status = defineUser();
 	while (status == 0) {
 		int maxLen = MAX_COMMAND_NAME + MAX_PATH_NAME + MAX_FILE_NAME;
