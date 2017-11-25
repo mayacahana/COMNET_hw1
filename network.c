184df6c8d3447d8bc3b929cb27cb564de417267c
mayacahana <maya.cahana24@gmail.com>
2017-11-25 23:58:03 +0200

    merge

--- network.c
+++ network.c
@@ -7,13 +7,14 @@
 #include "network.h"
 
 int send_command(int sckt, Message* msg_to_sent) {
-	int bytesLeft = strlen(msg_to_sent->arg1) + strlen(msg_to_sent->arg2) + 1; /* how many we have left to send */
+	int bytesLeft =  sizeof(msg_to_sent);
 	int total = 0; /* how many bytes we've sent */
 	int n, len = bytesLeft;
 	while (total < bytesLeft) {
 		n = send(sckt, msg_to_sent + total, bytesLeft, 0);
+		printf("sent %d bytes\n, socket = %d\n", n, sckt);
 		if (n < 0) {
-			printf("Send message with send() failed.");
+			printf("Send message with send() failed. %s\n", strerror(errno));
 			return 1;
 		}
 		total += n;
@@ -30,12 +31,14 @@ int receive_command(int sckt, Message* msg_recieved) {
 	int total = 0;
 	int bytesLeft = sizeof(msg_recieved);
 	int n, len = bytesLeft;
+	char* buffer;
+	buffer = (char *)malloc(sizeof(char)*MAX_FILE_SIZE+sizeof(Message));
 	while (total < bytesLeft) {
 		printf("Im before recv \n");
-		n = recv(sckt, msg_recieved, bytesLeft, 0);
-		printf("Im after recv \n");
+		n = recv(sckt, (void*) buffer, bytesLeft, 0);
+		print("bytes sent = %d\n", n);
 		if (n < 0) {
-			printf("Receive message with recv() failed.\n");
+			printf("Receive message with recv() failed.\n %s", strerror(errno));
 			return 1;
 		}
 		total += n;
@@ -46,6 +49,8 @@ int receive_command(int sckt, Message* msg_recieved) {
 		printf("Receive wrong amount of bytes in recv().");
 		return 1;
 	}
+	strcpy(msg_recieved->arg1, buffer);
+	free(buffer);
 	if (total > MAX_FILE_SIZE) {
 		printf("The message is too big. ");
 		return 1;
